5984e08351b58eaeedbcb86c45e156dcb0228d8b
Karl Snyder <karlsnyder0@gmail.com>
2023-01-25 11:55:44 -0500

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["src/**/*.jsx", "src/**/*.tsx", "src/**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+};
